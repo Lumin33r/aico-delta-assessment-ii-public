96199f1d53126e62add352bc4e86291d2228cc2d
Lumineer <troy.arris@gmail.com>
2025-12-16 22:22:12 -0500

    fix: Increase IMDSv2 hop limit to 2 for Docker containers

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -36,7 +36,7 @@ resource "aws_launch_template" "backend" {
   metadata_options {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
-    http_put_response_hop_limit = 1
+    http_put_response_hop_limit = 2
   }
 
   monitoring {
